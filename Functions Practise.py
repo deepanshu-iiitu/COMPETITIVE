2869585a6341a0696e517f6715bc3e0b4e3692c7
Deepanshu Bansal <deepanshu.iiitu@gmail.com>
2018-07-30 23:52:53 +0530

    30-07-2018

---
+++ Functions Practise.py
@@ -0,0 +1,12 @@
+def simple():
+    print("I am a function")
+def rupee_convertor(inr):
+    usd = inr / 68
+    print(usd)
+def allowed_dating_age(my_age):
+    girls_age = my_age/2 + 7
+    return girls_age
+simple()
+rupee_convertor(1000)
+age = allowed_dating_age(19)
+print(age)
