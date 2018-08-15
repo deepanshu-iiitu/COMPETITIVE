e6ea872b7e5fde4e2cd018b33b808d4802f073e5
Deepanshu Bansal <deepanshu.iiitu@gmail.com>
2018-08-15 17:33:26 +0530

    15/8/18

---
+++ Love choice.py
@@ -0,0 +1,10 @@
+def choice(yc ="still confused"):
+    if yc == 'y':
+        print("You love her")
+    elif yc == 'n':
+        print("Think again")
+    else:
+        print(yc)
+choice('y')
+choice('n')
+choice()
---
+++ keyword arguments.py
@@ -0,0 +1,4 @@
+def agent(name = 'admin', marks = 0):
+    print("Hi " + name + ". You scored", marks)
+agent()
+agent(name = 'bond', marks = 57)
